de66e96f1f574e5b84f5479f72c80a3dde04b0af
unknown <miss-ndeya>
2023-10-07 23:19:07 +0000

    ajout exo3

--- 30-Days-Of-JavaScript/04_Day_Conditionals/04_day_starter/Exercice/script/Exercice3.js
+++ 30-Days-Of-JavaScript/04_Day_Conditionals/04_day_starter/Exercice/script/Exercice3.js
@@ -1,4 +1,8 @@
 let mois = prompt("Entrez un mois");
+let année = prompt("Entrez l'année")
+let estBissextile = (année % 4 === 0 && (année % 100 !== 0 || année % 400 === 0));
+estBissextile = estBissextile ? 29 : 28;
+
 switch (mois) {
   case "janvier":
   case "mars":
@@ -16,8 +20,11 @@ switch (mois) {
     console.log(`${mois} compte 30 jours.`);
     break;
   case "fevrier":
-    console.log(`${mois} compte 28 jours.`);
+    console.log(`${mois} compte ${estBissextile} jours.`);
     break;
   default:
     console.log("Saisi un mois valid");
 }
+
+
+
